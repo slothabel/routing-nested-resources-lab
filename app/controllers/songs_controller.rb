1d54d551ae26589a811a874edd1a44793cbf27cd
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-02 17:02:55 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,17 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
+    # binding.pry
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else
+      @song = Song.find(params[:id])
+    end
+    
   end
 
   def new
